550291f9d138a61611feacb00688d708139cc026
Sergey Snegirev <sergei.snegiriov@yandex.ru>
2017-08-10 16:27:59 +0300

    Create lcs.cpp

---
+++ lcs.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <future>
+#include <thread>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	vector<vector<int> > d;
+	string a, b;
+
+	cin >> a >> b;
+
+	for (int i = 0; i < a.length(); ++i)
+	{
+		d.push_back(vector<int>(b.length(), 0));
+	}
+	
+	for (int i = 0; i < a.length(); ++i)
+	{
+		for (int j = 0; j < b.length(); ++j)
+		{
+			if (a[i] == a[j])
+			{
+				if (i > 0 && j > 0)
+				{
+					d[i][j] = d[i - 1][j - 1] + 1;
+				}
+				else
+				{
+					d[i][j] = 1;
+				}
+			}
+			else
+			{
+				if (i > 0 && j > 0)
+				{
+					d[i][j] = max(d[i - 1][j], d[i][j - 1]);
+				}
+				else
+				{
+					if (i == 0 && j > 0)
+					{
+						d[i][j] = d[i][j - 1];
+					}
+
+					if (j == 0 && i > 0)
+					{
+						d[i][j] = d[i - 1][j];
+					}
+				}
+			}
+		}
+	}
+
+	std::cout << d[a.length() - 1][b.length() - 1];
+
+	return 0;
+}
